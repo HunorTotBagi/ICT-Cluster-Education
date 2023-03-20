feca37e1b9518a8ccd0197e4c5293a322efa7d3f
Jovana Jovanovic <jovana.jovanovic@uns.ac.rs>
2023-03-20 12:10:01 +0100

    primer upotrebe css kontejnera

--- omni/src/css_example/layout/CSSExample_ListMainView.js
+++ omni/src/css_example/layout/CSSExample_ListMainView.js
@@ -1,36 +1,60 @@
-
-import './css_example_listmainview.css';
+import "./css_example_listmainview.css";
 const CSSExample_ListMainView = () => {
-
-   const openNav = () => {
-        document.getElementById("sidebar").style.width = "250px";
-        // document.getElementById("main").style.marginLeft = "250px";
-
-    }
-    const closeNav = () => {
-        document.getElementById("sidebar").style.width = "0";
-        // document.getElementById("main").style.marginLeft = "0";
-
-    }
-    return <div id='main'> 
-        <div className='header_list_view'>
-            <div>
-               <span onClick={openNav}> &#9776; Open </span>
-            </div>
-            <input type='text' placeholder='Search'/>
+    
+  return (
+    <div className="container_list_main_view">
+      <header className="container_list_main_view_header">
+        <div className="header_search">
+          <input type="text" placeholder="Search..." />
         </div>
-        <div className='left_side_bar_container' id='sidebar'>            
-                {/* side bar koji ce se prikazati sa strane */}
-                <a href="javascript:void(0)" class="closebtn" onClick={closeNav}>&times;</a>
-                <a href="#">About</a>
-                <a href="#">Services</a>
-                <a href="#">Clients</a>
-                <a href="#">Contact</a>          
+        <div className="header_avatar">&#9786;</div>
+      </header>
+      <aside className="container_list_main_view_sidenav" id="sidenav">
+        <ul className="container_list_main_view_sidenav_list">
+          <li className="container_list_main_view_sidenav_list_item">Item 1</li>
+          <li className="container_list_main_view_sidenav_list_item">Item 2</li>
+          <li className="container_list_main_view_sidenav_list_item">Item 3</li>
+          <li className="container_list_main_view_sidenav_list_item">Item 4</li>
+        </ul>
+      </aside>
+      <main className="container_list_main_view_main">
+        <div className="container_list_main_view_main_header">
+          <div className="container_list_main_view_main_header_heading">
+            Hello user
+          </div>
+          <div className="container_list_main_view_main_header_updates">
+            Recent Items
+          </div>
         </div>
-        <div className="list_view">
+        <div className="container_list_main_view_main_overview">
+          <div class="overviewcard">
+            <div class="overviewcard__icon">Overview</div>
+            <div class="overviewcard__info">Card</div>
+          </div>
+          <div class="overviewcard">
+            <div class="overviewcard__icon">Overview</div>
+            <div class="overviewcard__info">Card</div>
+          </div>
+          <div class="overviewcard">
+            <div class="overviewcard__icon">Overview</div>
+            <div class="overviewcard__info">Card</div>
+          </div>
+          <div class="overviewcard">
+            <div class="overviewcard__icon">Overview</div>
+            <div class="overviewcard__info">Card</div>
+          </div>
 
+        
         </div>
+        <div className="container_list_main_view_main_cards">
+            <div className="card">Card</div>
+            <div className="card">Card</div>
+            <div className="card">Card</div>
+          </div>
+      </main>
+      <footer className="container_list_main_view_footer"></footer>
     </div>
-}
+  );
+};
 
 export default CSSExample_ListMainView;
