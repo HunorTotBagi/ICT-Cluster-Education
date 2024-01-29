d88d3ed8ef8ef762d325d50b3c41805dc6b771f5
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-01-29 11:20:01 +0100

    add: functionality for user to enter the length of the array

--- src/nizovi/Zadatak3_nizovi.java
+++ src/nizovi/Zadatak3_nizovi.java
@@ -4,15 +4,21 @@ public class Zadatak3_nizovi {
 
 	public static void main(String[] args) {
 
-		final int numberOfElements = 12;
+		final int upperBound = 12;
+		int maxNumberOfElements = -1;
+
+		while (maxNumberOfElements < 0 || upperBound < maxNumberOfElements) {
+			System.out.printf("Enter the length of the array. The number should be 0 < n <= %d. %n", upperBound);
+			maxNumberOfElements = TextIO.getlnInt();
+		}
 
 		int counterA = 0;
 		int counterB = 0;
-		int[] array = new int[numberOfElements];
-		int[] oddArray = new int[numberOfElements];
-		int[] evenArray = new int[numberOfElements];
+		int[] array = new int[maxNumberOfElements];
+		int[] oddArray = new int[maxNumberOfElements];
+		int[] evenArray = new int[maxNumberOfElements];
 
-		for (int i = 0; i < numberOfElements; i++) {
+		for (int i = 0; i < maxNumberOfElements; i++) {
 			System.out.printf("Enter the %d. element of the array: %n", i + 1);
 			array[i] = TextIO.getlnInt();
 
@@ -25,12 +31,12 @@ public class Zadatak3_nizovi {
 			}
 		}
 
-		for (int i = 0; i < numberOfElements; i++) {
+		for (int i = 0; i < maxNumberOfElements; i++) {
 			if (evenArray[i] != 0)
 				System.out.printf("The %d. element of even array is: %d %n", i + 1, evenArray[i]);
 		}
 
-		for (int i = 0; i < numberOfElements; i++) {
+		for (int i = 0; i < maxNumberOfElements; i++) {
 			if (oddArray[i] != 0)
 				System.out.printf("The %d. element of odd array is: %d %n", i + 1, oddArray[i]);
 		}
