e1add4043780f098b98983b0c4a1a797e7004bd8
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-02-14 10:35:47 +0100

    chore: swap order of methods

--- Basics of Programming/first_obligatory_homework/CarPassageTracker.java
+++ Basics of Programming/first_obligatory_homework/CarPassageTracker.java
@@ -50,23 +50,21 @@ public class CarPassageTracker {
 		int ramp = getValidInput("Enter the ramp: ", 1, MAX_RAMPS);
 
 		int carPasses = carMatrix[day - 1][ramp - 1];
-		if (carPasses > 0) {
+		if (carPasses > 0)
 			overrideCarPasses(day, ramp, carPasses);
-		}
-		else {
+		else
 			inputCarPasses(day, ramp);
-		}
 	}
-
-	private static void inputCarPasses(int day, int ramp) {
-		int cars = getValidInput("Enter the number of cars: ", 0, 2147483647);
+	
+	private static void overrideCarPasses(int day, int ramp, int carPasses) {
+		System.out.printf("On day %d and ramp %d there already passed %d cars. ", day, ramp, carPasses);
+		int cars = getValidInput("Enter again the number of cars (previous data will be overriden by new entry): ", 0, 2147483647);
 		carMatrix[day - 1][ramp - 1] = cars;
 		System.out.printf("Successfully entered %d passed cars on day %d at ramp %d.%n", cars, day, ramp);
 	}
 
-	private static void overrideCarPasses(int day, int ramp, int carPasses) {
-		System.out.printf("On day %d and ramp %d there already passed %d cars. ", day, ramp, carPasses);
-		int cars = getValidInput("Enter again the number of cars (previous data will be overriden by new entry): ", 0, 2147483647);
+	private static void inputCarPasses(int day, int ramp) {
+		int cars = getValidInput("Enter the number of cars: ", 0, 2147483647);
 		carMatrix[day - 1][ramp - 1] = cars;
 		System.out.printf("Successfully entered %d passed cars on day %d at ramp %d.%n", cars, day, ramp);
 	}
