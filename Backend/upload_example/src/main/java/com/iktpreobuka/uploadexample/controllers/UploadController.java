c6e9850e5b0bed943c4743d3c228badac9f7997e
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-05-23 19:58:14 +0200

    upload

--- Backend/upload_example/src/main/java/com/iktpreobuka/uploadexample/controllers/UploadController.java
+++ Backend/upload_example/src/main/java/com/iktpreobuka/uploadexample/controllers/UploadController.java
@@ -2,6 +2,8 @@ package com.iktpreobuka.uploadexample.controllers;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,8 @@ public class UploadController {
 
 	@Autowired
 	private FileHandler fileHandler;
+	
+	private final Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String index() {
@@ -32,6 +36,13 @@ public class UploadController {
 	@RequestMapping(method = RequestMethod.POST, value = "/upload")
 	public String singleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
 		String result = null;
+		
+		logger.debug("This is a debug message");
+		logger.info("This is an info message");
+		logger.warn("This is a warn message");
+		logger.error("This is an error message");
+
+		
 		try {
 			result = fileHandler.singleFileUpload(file, redirectAttributes);
 		} catch (IOException e) {
