f1aa57d6bb97e80f5e139d7ba9c146a8b6cb3224
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-05-25 18:41:24 +0200

    remove @ResponseStatus

--- Backend/serialization_project1/src/main/java/com/iktpreobuka/serialization/controllers/GlobalExceptionHandler.java
+++ Backend/serialization_project1/src/main/java/com/iktpreobuka/serialization/controllers/GlobalExceptionHandler.java
@@ -4,14 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
         return new ResponseEntity<>("Invalid ID format", HttpStatus.BAD_REQUEST);
     }
